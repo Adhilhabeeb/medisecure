6ab2a3a149731420bfee1941c538e83e7b7a3a97
adhilhabeeb <132336648+Adhilhabeeb@users.noreply.github.com>
2025-12-07 19:14:28 +0530

    added osme in

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -44,7 +44,7 @@ let {messages}=data;
     return NextResponse.json("error", { status: 400 });
   }
 
-const lastMessage = messages[messages.length - 1];;
+const lastMessage ="hello mowne ";
     try {
 
       let gogleresponse= await googlemodel.generateContentStream({
