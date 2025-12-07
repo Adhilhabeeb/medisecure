de16a5861758ffc025a00b9845c9d76c87abb858
adhilhabeeb <132336648+Adhilhabeeb@users.noreply.github.com>
2025-12-07 19:08:01 +0530

    dd

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -44,7 +44,7 @@ let {messages}=data;
     return NextResponse.json("error", { status: 400 });
   }
 
-    const lastMessage = "adhil";
+const lastMessage = messages[messages.length - 1];;
     try {
 
       let gogleresponse= await googlemodel.generateContentStream({
